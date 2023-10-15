4ca547a84ff8904a2c966c185a4ac1a9a081f09d
jinhua.chen <jinnielazzy@gmail.com>
2023-10-15 16:23:55 -0700

    basic layout

--- App.tsx
+++ App.tsx
@@ -1,20 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import { Text, View } from 'react-native';
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+function Home() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>Home page, and pick bottle</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function BottleCreation() {
+  return (
+    <View>
+      <Text>Write bottle here</Text>
+    </View>
+  );
+}
+
+function MyBottles() {
+  return (
+    <View>
+      <Text>All my bottles</Text>
+    </View>
+  );
+}
+
+function BottleCollection() {
+  return (
+    <View>
+      <Text>All bottled I picked</Text>
+    </View>
+  );
+}
+
+function Settings() {
+  return (
+    <View>
+      <Text>Settings</Text>
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        {/* TODO: Passing additional props, Use React context and wrap the navigator with a context provider to pass data to the screens */}
+        <Tab.Screen component={Home} name="Home" options={{ title: 'Pick botte' }} />
+        <Tab.Screen component={BottleCreation} name="Write Bottle" />
+        <Tab.Screen component={MyBottles} name="My Bottles" />
+        <Tab.Screen component={BottleCollection} name="Collections" />
+        <Tab.Screen component={Settings} name="Settings" />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
